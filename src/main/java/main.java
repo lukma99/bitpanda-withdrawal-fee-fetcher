b817dc616e06e42e6de6e2a87999ff728c8c7661
lukma99 <lukma99@posteo.de>
2021-02-20 19:43:05 +0100

    Multiple changes: Completely use Bitpanda API, Support of ALL available Coins. Automatically refresh data every 30 seconds

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,21 +4,20 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collections;
 import javax.swing.*;
 
 
-public class main {
+public class main implements Job {
 
-    static int currentPrice; // Current price of bitcoin in EUR
+    static double currentPrice; // Current price of bitcoin in EUR
+    static String currentCoin = "BTC";
     static OkHttpClient client = new OkHttpClient();
-    final static String BITCOIN_PRICE_ENDPOINT = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur";
+    static String BITCOIN_PRICE_ENDPOINT = "https://api.bitpanda.com/v1/ticker";
     final static String BITPANDA_FEES_ENDPOINT = "https://www.bitpanda.com/de/limits";
 
 
@@ -32,9 +31,33 @@ public class main {
      *
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Initialize Gui
         initializeGui();
+
+        try {
+
+            // specify the job' s details..
+            JobDetail job = JobBuilder.newJob(main.class)
+                    .withIdentity("fetchData")
+                    .build();
+
+            // specify the running period of the job
+            Trigger trigger = TriggerBuilder.newTrigger()
+                    .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                            .withIntervalInSeconds(30)
+                            .repeatForever())
+                    .build();
+
+            //schedule the job
+            SchedulerFactory schFactory = new StdSchedulerFactory();
+            Scheduler sch = schFactory.getScheduler();
+            sch.start();
+            sch.scheduleJob(job, trigger);
+
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
 
 
         // Initial run
@@ -45,7 +68,7 @@ public class main {
     /**
      * Creates all elements for the GUI and arranges them
      */
-    public static void initializeGui() {
+    public static void initializeGui() throws IOException {
         // Create frame
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -59,15 +82,34 @@ public class main {
         frame.add(jPanelMain);
 
         JPanel jPanelTop = new JPanel();
-        jPanelTop.setLayout(new java.awt.GridLayout(3, 3));
+        jPanelTop.setLayout(new java.awt.GridLayout(4, 2));
         jPanelMain.add(jPanelTop);
 
         JPanel jPanelBottom = new JPanel();
-        jPanelBottom.setLayout(new java.awt.GridLayout(2, 1));
+        jPanelBottom.setLayout(new java.awt.GridLayout(1, 1));
         jPanelMain.add(jPanelBottom);
 
 
-        JLabel labelBtcPriceInEur = new JLabel("BTC Price in EUR");
+        JLabel labelCurrency = new JLabel("Coin");
+        jPanelTop.add(labelCurrency);
+
+        
+        ArrayList<String> comboBoxListe = getSupportedCurrencies();
+        Collections.sort(comboBoxListe);
+
+
+        JComboBox comboboxCoins = new JComboBox(comboBoxListe.toArray());
+        comboboxCoins.setSelectedItem("BTC");
+        comboboxCoins.addActionListener(e -> {
+            currentCoin = comboboxCoins.getSelectedItem().toString();
+            System.out.println(currentCoin);
+            run();
+        });
+
+        jPanelTop.add(comboboxCoins);
+
+
+        JLabel labelBtcPriceInEur = new JLabel("Price in EUR");
         jPanelTop.add(labelBtcPriceInEur);
 
         tfBtcPriceInEur.setEditable(false);
@@ -85,13 +127,14 @@ public class main {
         tfWithdrawalFeesInEur.setEditable(false);
         jPanelTop.add(tfWithdrawalFeesInEur);
 
-
+/*
         JButton buttonReload = new JButton("Reload Values");
         buttonReload.addActionListener(e -> run()); // Add Click Event
         jPanelBottom.add(buttonReload);
-
+*/
         // Disclaimer
         JLabel labelDisclaimer = new JLabel("<html>This tool is NOT an official tool by Bitpanda.<br>" +
+                "New data is fetched every 30 seconds.<br>" +
                 "No guarantee for correct data.<br></html>");
         jPanelBottom.add(labelDisclaimer);
 
@@ -103,6 +146,7 @@ public class main {
      * Method, which manages the calls to the other methods
      */
     public static void run() {
+        //BITCOIN_PRICE_ENDPOINT = "https://api.coingecko.com/api/v3/simple/price?ids="+ currentCoin +"&vs_currencies=eur";
         loadBitcoinPrice(new Callback() {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
@@ -135,8 +179,7 @@ public class main {
      */
     private static void parseBitcoinPrice(String str) {
         JSONObject jsonObject = new JSONObject(str);
-        currentPrice = jsonObject.getJSONObject("bitcoin").getInt("eur");
-        tfBtcPriceInEur.setText(currentPrice + " EUR");
+        currentPrice = jsonObject.getJSONObject(currentCoin).getDouble("EUR");
     }
 
 
@@ -162,18 +205,53 @@ public class main {
             String currentCurrency = row.getElementsByClass("table__row__cell table__row__cell--header ").get(0).text();
             String currentWithdrawalFee = row.getElementsByClass("table__row__cell ").get(3).text();
 
-            if (currentCurrency.equals("Bitcoin")) {
+            currentCurrency = currentWithdrawalFee.replaceAll("[^A-Za-z]+", "");
+
+            if (currentCurrency.equals(currentCoin)) {
                 String bitcoinWithdrawalFee = currentWithdrawalFee;
                 // Cut the " BTC" at the end of the String
-                bitcoinWithdrawalFee = bitcoinWithdrawalFee.substring(0, bitcoinWithdrawalFee.length() - 4);
+                bitcoinWithdrawalFee = bitcoinWithdrawalFee.replaceAll("[^\\d.]", "");
 
+                System.out.println(bitcoinWithdrawalFee);
                 double bitcoinWithdrawalFeeDouble = Double.parseDouble(bitcoinWithdrawalFee);
 
                 // set Text to current fees
-                tfWithdrawalFeesInBtc.setText(String.format("%.8f", bitcoinWithdrawalFeeDouble) + " BTC");
+                tfBtcPriceInEur.setText(currentPrice + " EUR");
+                tfWithdrawalFeesInBtc.setText(String.format("%.8f", bitcoinWithdrawalFeeDouble) + " " + currentCoin);
                 tfWithdrawalFeesInEur.setText(String.valueOf(String.format("%.2f", (bitcoinWithdrawalFeeDouble * currentPrice))) + " EUR");
             }
         }
     }
 
+
+    public static ArrayList<String> getSupportedCurrencies() throws IOException {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        // Set up connection to bitpanda limits site
+        Document doc = Jsoup.connect(BITPANDA_FEES_ENDPOINT).get();
+
+
+        // Extract rows of withdrawal table
+        Element table = doc.getElementById("transactioncosts").getElementsByClass("table").first();
+        Elements rows = table.getElementsByClass("table__row");
+
+        // Check all rows, if they contain the text "Bitcoin" (so other currencies are ignored)
+        for (int i = 1; i < rows.size(); i++) {
+            Element row = rows.get(i);
+
+            String currentCurrency = row.getElementsByClass("table__row__cell table__row__cell--header ").get(0).text();
+            String currentWithdrawalFee = row.getElementsByClass("table__row__cell ").get(3).text();
+
+            currentCurrency = currentWithdrawalFee.replaceAll("[^A-Za-z]+", "");
+
+            list.add(currentCurrency);
+        }
+        return list;
+    }
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        run();
+    }
 }
